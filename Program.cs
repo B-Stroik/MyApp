51ca3c80ab8032f7963ae528efe41003dc2cda1a
Brandon Stroik <stroikbrandon@gmail.com>
2025-09-22 10:44:17 -0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
-
-// Serve default documents (index.html) and static files from /wwwroot
 app.UseDefaultFiles();
 app.UseStaticFiles();
-
 app.Run();
