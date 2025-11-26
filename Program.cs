479394d2dd67466778c573e496092bdbcf452043
Brandon Stroik <stroikbrandon@gmail.com>
2025-11-26 15:01:01 -0600

    Update Program.cs
    With some AI help

--- Program.cs
+++ Program.cs
@@ -1,5 +1,31 @@
+using MyApp.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// 1) Register MVC
+builder.Services.AddControllersWithViews();
+
+// 2) Register your utility service for DI
+builder.Services.AddScoped<IUtilityService, UtilityService>();
+
 var app = builder.Build();
-app.UseDefaultFiles();
+
+// 3) Typical middleware pipeline
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
 app.UseStaticFiles();
+
+app.UseRouting();
+app.UseAuthorization();
+
+// 4) MVC route: default to Utility/Index
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Utility}/{action=Index}/{id?}");
+
 app.Run();
