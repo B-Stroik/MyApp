1b62fbe8b9276bd68df5618ef286e1be1e2528a1
Brandon Stroik <stroikbrandon@gmail.com>
2025-12-13 12:25:58 -0600

    Added a HomeController

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MyApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET /
+        [HttpGet("/")]
+        public IActionResult Index()
+        {
+            // Redirect root URL to Utility/Index
+            return RedirectToAction("Index", "Utility");
+        }
+    }
+}
